4fa253320a3a2a82383a97b8924229e3ebc012d6
Orkar504+ <jose7211963@gmail.com>
2024-07-02 16:11:17 -0600

    Add controlers, view and edit to web

--- app/Http/Controllers/ventaController.php
+++ app/Http/Controllers/ventaController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+    
 class ventaController extends Controller
 {
-    //
+    function calcPrecio(float $area, int $piso)
+    {
+        $costoExtra = 1.01;
+        $costXmetro2 = 1500;
+        $precio;
+
+        if ($piso == null || $piso == 1)
+        {
+            $precio = $costXmetro2 * $area ; 
+        } else
+        {
+            $precio = $costXmetro2 * $area ;
+            for ($i = 1; $i < $piso; $i++)
+            {
+                $precio = $precio * $costoExtra;
+            }
+        }
+
+        return view('venta', compact('area','piso','precio'));
+    }
+
+    function getArea(float $area)
+    {
+        $precio = 500;
+        $piso = 1;
+
+        return view('venta', compact('area','piso','precio'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ventaController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/venta/{area}/{piso$}',function()
+Route::get('/venta/{area}/{piso?}',[ventaController:: class,'calcPrecio' ]);
+#Route::get('/venta/{area}',[ventaController:: class,'getArea' ]);
+/*
+Route::get('/venta/{area}', function(float $area)
+
 {
-    echo "vista venta";
+    echo "El area del apartamento es:   ".$area;
 });
+*/
+
 Route::get('/renta',function()
 {
     echo "vista renta";
