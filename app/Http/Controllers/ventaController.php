239216e6c6d65294c7565c516a9099988d8dfb2a
Orkar504+ <jose7211963@gmail.com>
2024-07-02 16:52:16 -0600

    maintain simple

--- app/Http/Controllers/ventaController.php
+++ app/Http/Controllers/ventaController.php
@@ -16,25 +16,22 @@ class ventaController extends Controller
         if ($piso == null || $piso == 1)
         {
             $precio = $costXmetro2 * $area ; 
-            $controller->returnVentaView($area,$piso,$precio);
-        } elseif($piso >0 && $piso <13)
+            
+        } else
         {
             $precio = $costXmetro2 * $area ;
             for ($i = 1; $i < $piso; $i++)
             {
                 $precio = $precio * $costoExtra;
             }
-            $controller->returnVentaView($area,$piso,$precio);
+            
         }
-
-       
+         return view('venta', compact('area','piso','precio'));
+      
+   
     }
 
-    private function returnVentaView(float $area,int $piso,float $precio)
-    {
-        return view('venta', compact('area','piso','precio'));
 
-    }
 
     
 }
